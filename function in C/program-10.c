f67c792fbf35eaf7476b25db5a768b5f446c0dcd
Web-Developer-Hub <habibor144369@gmail.com>
2022-11-20 01:09:44 +0600

    move on dude

--- function in C/program-10.c
+++ function in C/program-10.c
@@ -1,1 +1,25 @@
 //check the number is prime or not in c program...
+
+#include <stdio.h>
+
+int is_prime(int p){
+
+}
+
+int main() {
+    int n, prime;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    prime = is_prime(n);
+
+    if(prime == 1){
+        printf("%d is a prime number! \n");
+    }
+    else{
+        printf("%d is not a prime number! \n");
+    }
+
+
+return 0;
+}
