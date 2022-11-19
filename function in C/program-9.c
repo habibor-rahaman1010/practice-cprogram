b6e7837ce80c0d94c3b4d88846d35f9d7dc82839
Web-Developer-Hub <habibor144369@gmail.com>
2022-11-20 00:56:17 +0600

    keep going programmer

--- function in C/program-9.c
+++ function in C/program-9.c
@@ -1,8 +1,25 @@
+//LCM in c program....
 
 #include <stdio.h>
 
-int main() {
+int LCM(int x, int y) {
+    int ans = 0, i;
 
+    for(i = x; i > 1; i--){
+        if(x % i == 0 && y % i == 0){
+            ans = i;
+            break;
+        }
+    }
+    return (x * y) / ans;
+}
+
+int main() {
+    int x, y;
+    printf("Enter the value of x and y: ");
+    scanf("%d %d", &x, &y);
+
+    printf("The LCM is: %d \n", LCM(x, y));
 
 return 0;
 }
