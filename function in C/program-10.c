98d35287fe43b4d0c8c7a3e25c27fdc32fd57bdc
Web-Developer-Hub <habibor144369@gmail.com>
2022-11-20 01:18:09 +0600

    move on dude

--- function in C/program-10.c
+++ function in C/program-10.c
@@ -3,7 +3,14 @@
 #include <stdio.h>
 
 int is_prime(int p){
+    int i, flag = 1;
 
+    for(i = 2; i < p; i++){
+        if(p % i == 0){
+            flag = 0;
+        }
+    }
+    return flag;
 }
 
 int main() {
@@ -14,10 +21,10 @@ int main() {
     prime = is_prime(n);
 
     if(prime == 1){
-        printf("%d is a prime number! \n");
+        printf("%d is a prime number! \n", n);
     }
     else{
-        printf("%d is not a prime number! \n");
+        printf("%d is not a prime number! \n", n);
     }
 
 
