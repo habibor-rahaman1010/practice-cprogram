573ae6a7d9e8747b6ac27196cc58f4d43190b961
Web-Developer-Hub <habibor144369@gmail.com>
2022-11-21 14:22:58 +0600

    im a programmer

--- function in C/program-20.c
+++ function in C/program-20.c
@@ -8,31 +8,48 @@
 
 #include <stdio.h>
 
-int is_seven() {
-
-
+int is_seven(int n) {
+    while(n > 0){
+        if(n % 10 == 7){
+            return 1;
+        }
+        else{
+           n /= 10;
+        }
+    }
+    return 0;
 }
 
-int digit_summation() {
-
-
+int digit_summation(int n) {
+    int sum = 0;
+    while(n > 0){
+        sum += n % 10;
+        n /= 10;
+    }
+    return sum;
 }
 
-int is_prime() {
-
-
+int is_prime(int n) {
+    int ld = n % 10;
+    if(ld == 2 || ld == 3 || ld == 5 || ld == 7){
+        return 1;
+    }
+    else{
+        return 0;
+    }
 }
 
 
 int main() {
     int n;
     printf("Enter the value of n: ");
+    scanf("%d", &n);
 
-    if(is_seven() == 1 && digit_summation() == 1 && is_prime() == 1){
-        printf("It\'s spacial number of Habib!");
+    if(is_seven(n) == 1 && digit_summation(n) > 10 && is_prime(n) == 1){
+        printf("It\'s spacial number of Habib! \n");
     }
     else{
-        printf("Dump number!");
+        printf("Dump number! \n");
     }
 
 return 0;
