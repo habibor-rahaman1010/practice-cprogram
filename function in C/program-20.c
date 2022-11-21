3a857f56bcce8de47011cefa98c497f126a767ae
Web-Developer-Hub <habibor144369@gmail.com>
2022-11-21 13:58:03 +0600

    Hello Programmer

---
+++ function in C/program-20.c
@@ -0,0 +1,31 @@
+//special number in c program...
+
+/*
+    7 must be present...
+    digit summation must be more then 10...
+    last digit muse be prime
+*/
+
+#include <stdio.h>
+
+int is_seven() {
+
+
+}
+
+int digi_summation() {
+
+
+}
+
+int is_prime() {
+
+
+}
+
+
+int main() {
+
+
+return 0;
+}
