d60cec22e9b1c338f8eff1dac1cfb87d85dfd81f
Web-Developer-Hub <habibor144369@gmail.com>
2022-11-21 12:59:54 +0600

    ok man

--- function in C/program-15.c
+++ function in C/program-15.c
@@ -1,1 +1,39 @@
+// Nested function in c program....
 
+#include <stdio.h>
+#include <math.h>
+
+float f(int a, int b);
+int absolute(int n);
+int squear(int m);
+
+int main() {
+    int x, y;
+    printf("Enter the value of (x and y): ");
+    scanf("%d %d", &x, &y);
+
+    float ans = f(x, y);
+    printf("%f", ans);
+
+
+return 0;
+}
+
+
+float f(int a, int b){
+    int tampans = absolute(a) + squear(b);
+    return sqrt(tampans);
+}
+
+int absolute(int n){
+    if(n >= 0){
+        return n;
+    }
+    else{
+        return (-1) * n;
+    }
+}
+
+int squear(int m){
+    return (m + 4) * (m + 4);
+}
