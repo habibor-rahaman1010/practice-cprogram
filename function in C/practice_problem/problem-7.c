e35c7650fefdb6f37c7ea3429934e3b21a892abb
Web-Developer-Hub <habibor144369@gmail.com>
2022-11-26 19:46:23 +0600

    ok

--- function in C/practice_problem/problem-7.c
+++ function in C/practice_problem/problem-7.c
@@ -1,1 +1,37 @@
-//
+// calculator in c program....
+
+#include <stdio.h>
+
+void calculator(int x, int y, char symbol) {
+    int result;
+
+    if(symbol == '+'){
+        result = x + y;
+        printf("Summation is: %d \n", result);
+    }
+    else if(symbol == '-'){
+        result = x - y;
+        printf("Subtract is: %d \n", result);
+    }
+    else if(symbol == '*'){
+        result = x * y;
+        printf("Multiplication is: %d \n", result);
+    }
+    else if(symbol == '/'){
+        result = x / y;
+        printf("Divide is: %d \n", result);
+    }
+}
+
+int main() {
+    int a, b;
+    char ch;
+    printf("Enter your operator: ");
+    scanf("%c", &ch);
+    printf("Enter the value of (a and b): ");
+    scanf("%d %d", &a, &b);
+
+    calculator(a, b, ch);
+
+return 0;
+}
