6b1924485e2b9d0ec7b73a68c41d4e185858a6ec
Web-Developer-Hub <habibor144369@gmail.com>
2022-11-27 00:11:03 +0600

    ok done programmer

--- YCPC by Phitron 1st Round/problem-7.c
+++ YCPC by Phitron 1st Round/problem-7.c
@@ -1,9 +1,39 @@
+// vowel and consonant in c program....
 
 #include <stdio.h>
+
+int is_vowel(char ch) {
+    if(ch == 'a' || ch == 'A'){
+        return 1;
+    }
+    else if(ch == 'e' || ch == 'E'){
+        return 1;
+    }
+    else if(ch == 'i' || ch == 'I'){
+        return 1;
+    }
+    else if(ch == 'o' || ch == 'O'){
+        return 1;
+    }
+    else if(ch == 'u' || ch == 'U'){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+
+}
 
 int main() {
     char ch;
     printf("Enter your character: ");
     scanf("%c", &ch);
+
+    if(is_vowel(ch)){
+        printf("Vowel \n");
+    }
+    else{
+        printf("Consonant \n");
+    }
 
 }
