7a5a045087ab9753d78a2e779297a11a5a8352d3
Web-Developer-Hub <habibor144369@gmail.com>
2022-11-26 19:13:44 +0600

    hello programmer

--- function in C/practice_problem/problem-6.c
+++ function in C/practice_problem/problem-6.c
@@ -1,10 +1,10 @@
-// even and odd number in c program...
+//odd number in c program...
 
 #include <stdio.h>
 
 void even(int n) {
     for(int i = 1; i < n; i++){
-        if(i % 2 == 0){
+        if(i % 2 != 0){
             printf("%d ", i);
         }
     }
