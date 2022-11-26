52e7bc7d4f1a2da9cca9263aade93e09b9d22cd4
Web-Developer-Hub <habibor144369@gmail.com>
2022-11-26 20:44:55 +0600

    hello programmer

--- function in C/practice_problem/problem -8.c
+++ function in C/practice_problem/problem -8.c
@@ -1,8 +1,21 @@
+//Write a C program to take one positive integer N as input and print all divisors of N.
 
 #include <stdio.h>
 
+void divisor(int n) {
+    int i;
+    for(i = 1; i <= n; i++){
+        if(n % i == 0){
+            printf("%d ", i);
+        }
+    }
+}
+
 int main() {
-    printf("Hello World");
+    int n;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+    divisor(n);
 
 return 0;
 }
