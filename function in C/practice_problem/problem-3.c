b1c5b61c79d9e308b573a7e3ff6d177c801e1778
Web-Developer-Hub <habibor144369@gmail.com>
2022-11-26 18:51:39 +0600

    ok done

--- function in C/practice_problem/problem-3.c
+++ function in C/practice_problem/problem-3.c
@@ -1,11 +1,10 @@
 //pyramid pattern in c program...
 
 #include <stdio.h>
-int main() {
-   int rows, i, j, space;
-   printf("Enter the number of rows: ");
-   scanf("%d", &rows);
-   for (i = rows; i >= 1; --i) {
+
+void pyramid(int rows) {
+     int i, j, space;
+    for (i = rows; i >= 1; --i) {
       for (space = 0; space < rows - i; ++space)
          printf("  ");
       for (j = i; j <= 2 * i - 1; ++j)
@@ -14,6 +13,14 @@ int main() {
          printf("* ");
       printf("\n");
    }
+}
+
+int main() {
+   int rows;
+   printf("Enter the number of rows: ");
+   scanf("%d", &rows);
+   pyramid(rows);
+
    return 0;
 }
 
