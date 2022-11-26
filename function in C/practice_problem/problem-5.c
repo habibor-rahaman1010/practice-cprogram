bf695ef9c265eaf7b4e363757ea2d8cb8ae6a207
Web-Developer-Hub <habibor144369@gmail.com>
2022-11-26 19:11:37 +0600

    ok man

---
+++ function in C/practice_problem/problem-5.c
@@ -0,0 +1,20 @@
+// even and odd number in c program...
+
+#include <stdio.h>
+
+void even(int n) {
+    for(int i = 1; i < n; i++){
+        if(i % 2 == 0){
+            printf("%d ", i);
+        }
+    }
+}
+
+int main() {
+    int n;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+    even(n);
+
+return 0;
+}
