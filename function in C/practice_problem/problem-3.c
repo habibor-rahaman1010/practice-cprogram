8822b040408d9452cae77e1fb2a9b6f138394e08
Web-Developer-Hub <habibor144369@gmail.com>
2022-11-26 18:44:49 +0600

    hello programmer

--- function in C/practice_problem/problem-3.c
+++ function in C/practice_problem/problem-3.c
@@ -2,17 +2,16 @@
 
 #include <stdio.h>
 int main() {
-   int i, space, rows, k = 0;
+   int rows, i, j, space;
    printf("Enter the number of rows: ");
    scanf("%d", &rows);
-   for (i = 1; i <= rows; ++i, k = 0) {
-      for (space = 1; space <= rows - i; ++space) {
+   for (i = rows; i >= 1; --i) {
+      for (space = 0; space < rows - i; ++space)
          printf("  ");
-      }
-      while (k != 2 * i - 1) {
+      for (j = i; j <= 2 * i - 1; ++j)
          printf("* ");
-         ++k;
-      }
+      for (j = 0; j < i - 1; ++j)
+         printf("* ");
       printf("\n");
    }
    return 0;
