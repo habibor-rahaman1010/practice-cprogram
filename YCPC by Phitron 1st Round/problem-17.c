b0bb48e301688e12cc05a42018aef6b20e9a03c1
Web-Developer-Hub <habibor144369@gmail.com>
2022-11-28 11:06:07 +0600

    ok done

--- YCPC by Phitron 1st Round/problem-17.c
+++ YCPC by Phitron 1st Round/problem-17.c
@@ -1,1 +1,45 @@
+// matrix multiplication in c program...
 
+#include <stdio.h>
+
+void matrix_multiply() {
+    int row, col;
+    printf("Enter your (row and col it\'s must be squear): ");
+    scanf("%d %d", &row, &col);
+
+    int number1[row][col], number2[row][col];
+    int i, j;
+    printf("Enter element of number1 array: ");
+    for(i = 0; i < row; i++){
+        for(j = 0; j < col; j++){
+            scanf("%d", &number1[i][j]);
+        }
+    }
+
+    printf("Enter element of number2 array: ");
+    for(i = 0; i < row; i++){
+        for(j = 0; j < col; j++){
+            scanf("%d", &number2[i][j]);
+        }
+    }
+
+    //multiplication 2 array here...
+    multiply = 1;
+    for(i = 1; i <= row; i++){
+        for(j = 1; j <= col; j++){
+           multiply = number1[i][j] * number2[i][j];
+           printf("%d ", multiply);
+        }
+        printf("\n");
+    }
+
+return;
+}
+
+
+int main() {
+
+    matrix_multiply();
+
+return 0;
+}
