d0a54b2a50250670a11c0932c24d18e978274eb4
Web-Developer-Hub <habibor144369@gmail.com>
2022-11-28 11:45:15 +0600

    ok done

--- YCPC by Phitron 1st Round/problem-17.c
+++ YCPC by Phitron 1st Round/problem-17.c
@@ -10,15 +10,15 @@ void matrix_multiply() {
     int number1[row][col], number2[row][col];
     int i, j;
     printf("Enter element of number1 array: ");
-    for(i = 0; i < row; i++){
-        for(j = 0; j < col; j++){
+    for(i = 1; i <= row; i++){
+        for(j = 1; j <= col; j++){
             scanf("%d", &number1[i][j]);
         }
     }
 
     printf("Enter element of number2 array: ");
-    for(i = 0; i < row; i++){
-        for(j = 0; j < col; j++){
+    for(i = 1; i <= row; i++){
+        for(j = 1; j <= col; j++){
             scanf("%d", &number2[i][j]);
         }
     }
