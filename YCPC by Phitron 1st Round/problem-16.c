35a014296357992db02250feb724638bdeaef8d8
Web-Developer-Hub <habibor144369@gmail.com>
2022-11-28 10:27:24 +0600

    ok done

--- YCPC by Phitron 1st Round/problem-16.c
+++ YCPC by Phitron 1st Round/problem-16.c
@@ -1,7 +1,37 @@
+// sort in c program
+
 #include <stdio.h>
 
+void array_sort(int number[], int N) {
+    int i, j, tamp;
+
+    for(i = 0; i < N; i++){
+        for(j  = 0; i < j - 1; j++){
+            if(number[j] > number[j + 1]){
+                tamp = number[j];
+                number[j] = number[j + 1];
+                number[j + 1] = tamp;
+            }
+        }
+    }
+    for(i = 0; i < N; i++){
+        printf("%d ", number[i]);
+    }
+
+}
+
 int main() {
-    printf("Knowledge is power.");
+    int N;
+    printf("Enter the value of N: ");
+    scanf("%d", &N);
+
+    int number[N];
+    printf("Enter element of number array: ");
+    for(int i = 0; i < N; i++){
+        scanf("%d", &number[i]);
+    }
+
+    array_sort(number, N);
 
 return 0;
 }
