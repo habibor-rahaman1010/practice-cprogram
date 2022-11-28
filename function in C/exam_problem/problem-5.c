51794efa1b63207f27b31faad296f8279c9ca83c
Web-Developer-Hub <habibor144369@gmail.com>
2022-11-28 14:54:49 +0600

    programmer is very smart

--- function in C/exam_problem/problem-5.c
+++ function in C/exam_problem/problem-5.c
@@ -1,1 +1,17 @@
 
+#include <stdio.h>
+
+int add_three_nums(int a, int b, int c = 0) {
+    return a + b + c;
+}
+
+int main() {
+    int a, b;
+    printf("Enter the value of (a and b): ");
+    scanf("%d %d", &a, &b);
+
+    int result = add_three_nums(a, b);
+    printf("Summation is: %d", result);
+
+return 0;
+}
