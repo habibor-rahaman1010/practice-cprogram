d5472fec3cf5da013e252f7ab7b6e0037d76a7a5
Web-Developer-Hub <habibor144369@gmail.com>
2022-11-28 09:27:23 +0600

    Hello Programmer

--- YCPC by Phitron 1st Round/problem-15.c
+++ YCPC by Phitron 1st Round/problem-15.c
@@ -9,21 +9,18 @@ int main()
 
     scanf("%s", &s1);
 
-    for(i=0; i<N-1; i++)
-    {
+    for(i = 0; i < N - 1; i++){
         char s2[50];
         scanf("%s ", &s2);
         // store length of s1 in the length variable
         length = 0;
-        while (s1[length] != '\0')
-        {
-            ++length;
+        while (s1[length] != '\0'){
+            length++;
         }
         s1[length] = ' ';
-        ++length;
+        length++;
         // concatenate s2 to s1
-        for (j = 0; s2[j] != '\0'; ++j, ++length)
-        {
+        for (j = 0; s2[j] != '\0'; j++, length++){
             s1[length] = s2[j];
         }
 
