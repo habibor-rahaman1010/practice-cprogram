5e3da67727b093b4228b859a346b97427f945af0
Web-Developer-Hub <habibor144369@gmail.com>
2022-11-28 12:17:15 +0600

    programmer always smart in the entire world

--- function in C/exam_problem/problem-1.c
+++ function in C/exam_problem/problem-1.c
@@ -1,9 +1,27 @@
+//capital case lower and lower case capital in c program....
+
 #include <stdio.h>
+#include <string.h>
+
+void chnage_small(char str[100]) {
+    int i, len = strlen(str);
+    for(i = 0; i < len; i++){
+        if(str[i] >= 'a' && str[i] <= 'z'){
+            printf("%c", str[i] -= 32);
+        }
+        else if(str[i] >= 'A' && str[i] <= 'Z'){
+            printf("%c", str[i] += 32);
+        }
+    }
+
+return;
+}
 
 int main() {
     char str[100];
     scanf("%s", str);
-    printf("%s", str);
+
+    chnage_small(str);
 
 return 0;
 }
