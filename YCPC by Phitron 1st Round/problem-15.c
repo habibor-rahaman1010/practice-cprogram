109af5ac9ed1a5f6184dbb45cbea5984c788d0fb
Web-Developer-Hub <habibor144369@gmail.com>
2022-11-28 09:20:56 +0600

    ok programmer

--- YCPC by Phitron 1st Round/problem-15.c
+++ YCPC by Phitron 1st Round/problem-15.c
@@ -1,14 +1,44 @@
 //concatenation  in c program...
 
 #include <stdio.h>
-#include <string.h>
 
-void string_concate() {
+void string_concatenation(char s1[1000], int N) {
+    int length, j, i;
+    for(i = 0; i < N - 1; i++){
+        char s2[50];
+        scanf("%s", s2);
+        // store length of s1 in the length variable
+        length = 0;
+        while (s1[length] != '\0'){
+            length++;
+        }
+        s1[length] = ' ';
+        length++;
+        // concatenate s2 to s1
+        for (j = 0; s2[j] != '\0'; j++, length++){
+            s1[length] = s2[j];
+        }
 
+        // terminating the s1 string
+        s1[length] = '\0';
+    }
+
+    printf("After concatenation: ");
+    puts(s1);
+
+return;
 }
 
 int main() {
-    char str[100];
+    int N;
+    char s1[1000];
+    printf("Enter your string number: ");
+    scanf("%d", &N);
+
+    printf("Enter string: ");
+    scanf("%s", s1);
+
+    string_concatenation(s1, N);
 
 return 0;
 }
