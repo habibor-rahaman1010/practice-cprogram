249ab0a33b572c7e740b66fbd07f72fdefa5e283
Web-Developer-Hub <habibor144369@gmail.com>
2022-11-28 13:36:58 +0600

    ok

--- function in C/exam_problem/problem-3.c
+++ function in C/exam_problem/problem-3.c
@@ -1,10 +1,30 @@
 #include <stdio.h>
 
-int main() {
-    char number[100];
-    printf("Enter your digit: ");
-    scanf("%s", number);
+void myfunc(char digit[100]) {
+    int i = 0, a = 0; b = 0; c = 0;
 
+    while(digit[i] != '\0') {
+        if(digit[i] == '1'){
+            a = 1;
+        }
+        else if(digit[i] == '7'){
+            b = 1;
+        }
+        else if(digit[i] == '9'){
+            c = 1;
+        }
+
+        i++;
+    }
+
+return;
+}
+
+int main() {
+    char digit[100];
+    printf("Enter your digit: ");
+    scanf("%s", digit);
+    myfunc(digit);
 
 return 0;
 }
