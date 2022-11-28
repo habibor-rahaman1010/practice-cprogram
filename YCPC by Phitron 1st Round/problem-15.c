f9a363f8af838b60dc05be51eb965c2371154951
Web-Developer-Hub <habibor144369@gmail.com>
2022-11-28 09:32:30 +0600

    Hello Programmer

--- YCPC by Phitron 1st Round/problem-15.c
+++ YCPC by Phitron 1st Round/problem-15.c
@@ -1,17 +1,10 @@
 #include <stdio.h>
 
-int main()
-{
-    char s1[1000];
-
-    int length, j, i, N;
-    scanf("%d", &N);
-
-    scanf("%s", &s1);
-
+void string_concatenation(char s1[1000], int N) {
+    int length, j, i;
     for(i = 0; i < N - 1; i++){
         char s2[50];
-        scanf("%s ", &s2);
+        scanf("%s", s2);
         // store length of s1 in the length variable
         length = 0;
         while (s1[length] != '\0'){
@@ -31,5 +24,22 @@ int main()
     printf("After concatenation: ");
     puts(s1);
 
-    return 0;
+return;
+}
+
+int main()
+{
+    char s1[1000];
+
+    int length, j, i, N;
+
+    printf("Enter your string size: ");
+    scanf("%d", &N);
+
+    printf("Enter your string: ");
+    scanf("%s", s1);
+
+    string_concatenation(s1, N);
+
+return 0;
 }
