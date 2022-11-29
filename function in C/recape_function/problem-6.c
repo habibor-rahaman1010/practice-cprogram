54f118233e507247347907ac0bb80dbb3a0dbef2
habibor144369 <onilhabib1@gmail.com>
2022-11-29 20:35:40 +0600

    ok

--- function in C/recape_function/problem-6.c
+++ function in C/recape_function/problem-6.c
@@ -1,1 +1,25 @@
+// factorial im c program use recursion...
 
+
+#include <stdio.h>
+
+int factorial(int n) {
+    if(n == 0){
+        return 1;
+    }
+    else{
+        return n * factorial(n - 1);
+    }
+
+}
+
+int main() {
+    int n;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    int result = factorial(n);
+    printf("factorial is: %d", result);
+
+return 0;
+}
