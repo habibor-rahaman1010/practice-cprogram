37111ee86272f758ee225a3ba030139a0a882d90
habibor144369 <onilhabib1@gmail.com>
2022-11-29 17:09:52 +0600

    hello programmer

--- function in C/exam_problem/program-9.c
+++ function in C/exam_problem/program-9.c
@@ -1,1 +1,27 @@
+//Write a C program that will take a 3x3 matrix as input and then print the transpose of that matrix.
 
+
+#include <stdio.h>
+
+int main(){
+    int N=3, M=3, i, j;
+
+    int a[N][M];
+    printf("Provide %dx%d matrix\n", N, M);
+
+    for(i=0; i<N; i++){
+        for(j=0; j<M; j++){
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    printf("Transpose Matrix\n");
+    for(i=0; i<N; i++){
+        for(j=0; j<M; j++){
+            printf("%d ", a[j][i]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
