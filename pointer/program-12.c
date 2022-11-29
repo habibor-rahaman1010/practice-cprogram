10f54036be89ae95c1953325dfc5fc39a1020fb9
habibor144369 <onilhabib1@gmail.com>
2022-11-29 22:03:19 +0600

    Hello Developer

--- pointer/program-12.c
+++ pointer/program-12.c
@@ -6,9 +6,11 @@ int main() {
     int ara[10] = {12, 34, 35, 56, 67, 78, 87, 99, 43, 54};
 
     int i;
+    int *p = ara;
     for(i = 0; i < 10; i++){
-        printf("%d  %p  %d\n", *(ara + i), (ara + i), (ara + i));
+        printf("%d  %p  %d\n", *(p + i), (p + i), (p + i));
     }
 
 return 0;
 }
+
