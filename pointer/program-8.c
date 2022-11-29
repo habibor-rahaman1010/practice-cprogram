c56f1adbfceaa072596d4ce8c9310cbd09eed1f1
habibor144369 <onilhabib1@gmail.com>
2022-11-29 21:20:58 +0600

    ok

--- pointer/program-8.c
+++ pointer/program-8.c
@@ -1,1 +1,28 @@
+//pointer in c program....
+
+#include <stdio.h>
+
+int main() {
+    long long int ara[10] = {23, 45, 67, 88, 98, 34, 23, 56, 77, 52};
+
+    printf("%p \n", &ara[0]);
+    printf("%p \n", &ara[1]);
+    printf("%p \n", &ara[2]);
+    printf("%p \n", &ara[3]);
+    printf("%p \n", &ara[4]);
+
+
+    printf("\n");
+
+    printf("%d \n", &ara[0]);
+    printf("%d \n", &ara[1]);
+    printf("%d \n", &ara[2]);
+    printf("%d \n", &ara[3]);
+    printf("%d \n", &ara[4]);
+
+return 0;
+}
+
+
+
 
