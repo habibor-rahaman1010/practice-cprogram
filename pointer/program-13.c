7dc562c8123108874af3cff3ee3dd6d5843384a8
habibor144369 <onilhabib1@gmail.com>
2022-11-29 22:10:49 +0600

    Hello Developer

--- pointer/program-13.c
+++ pointer/program-13.c
@@ -1,10 +1,16 @@
 // pointer value replace in c program...
 
-
 #include <stdio.h>
 
 int main() {
+    int a = 12;
 
+    int *p;
+    p = &a;
+
+    *p = 14;
+
+    printf("%d \n", a);
 
 return 0;
 }
