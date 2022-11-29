0a9c36078b0f7164823a2c5bcfae897e52c3b7b7
habibor144369 <onilhabib1@gmail.com>
2022-11-29 17:00:27 +0600

    ok

--- function in C/exam_problem/problem-7.c
+++ function in C/exam_problem/problem-7.c
@@ -1,20 +1,21 @@
+//Write a C function that will take an array of integers as an argument and return the median value of the array.
 
 #include <stdio.h>
 
 int main(){
-   int i=0, j=0, N;
+   int i = 0, j = 0, N;
    printf("Give array size = ");
    scanf("%d", &N);
    int a[N];
    printf("Provide total %d int numbers: ", N);
 
-   for(i=0; i<N; i++){
+   for(i = 0; i < N; i++){
       scanf("%d", &a[i]);
    }
 
    int temp;
-   for(i=0; i<N-1; i++){
-      for(j=i; j<N; j++){
+   for(i = 0; i < N - 1; i++){
+      for(j = i; j < N; j++){
          if(a[j] < a[i]){
             temp = a[j];
             a[j] = a[i];
@@ -27,13 +28,14 @@ int main(){
       printf("%d ", a[i]);
    }
 
-   if(N%2==1)
+   if(N % 2 == 1){
       printf("Median = %d", a[N/2]);
-   else
-   {
+   }
+   else{
       int half = N/2;
       float sum = a[half] + a[half+1];
       printf("Median = %.2f", sum/2);
    }
-   return 0;
+
+return 0;
 }
