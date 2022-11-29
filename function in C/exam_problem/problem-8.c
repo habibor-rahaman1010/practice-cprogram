7ab7e4c8e02d5dd63143294605cc8426c863df12
habibor144369 <onilhabib1@gmail.com>
2022-11-29 17:05:46 +0600

    ok done

---
+++ function in C/exam_problem/problem-8.c
@@ -0,0 +1,42 @@
+//character shift in c program..
+
+#include <stdio.h>
+
+void char_shift(char s[100],int N);
+
+int main(){
+    int N;
+    char s[100];
+    printf("Enter your string: ");
+    scanf("%s", s);
+
+    printf("Enter the shift number: ");
+    scanf("%d", &N);
+
+    char_shift(s, N);
+
+    return 0;
+}
+
+void char_shift(char s[100],int N){
+   int i=0;
+   while(s[i]!= '\0')
+   {
+      if(s[i]>= 'a' && s[i]<= 'z')
+      {
+         int val = s[i] += N;
+         if(val > 122)
+         {
+            int v = s[i] - 122;
+            int c = 97 + v -1;
+            printf("%c", c);
+         }
+         else
+         {
+            printf("%c", val);
+         }
+      }
+      i++;
+   }
+}
+
