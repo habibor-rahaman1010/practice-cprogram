56c456e93a03e7dbdc66d8fe60834109016ea21d
habibor144369 <onilhabib1@gmail.com>
2022-11-29 21:07:27 +0600

    ok

--- pointer/program-1.c
+++ pointer/program-1.c
@@ -3,7 +3,19 @@
 #include <stdio.h>
 
 int main() {
+    int a, b, c, d;
 
+    printf("%p \n", &a);
+    printf("%p \n", &b);
+    printf("%p \n", &c);
+    printf("%p \n", &d);
+
+    printf("\n");
+
+    printf("%d \n", &a);
+    printf("%d \n", &b);
+    printf("%d \n", &c);
+    printf("%d \n", &d);
 
 return 0;
 }
