418a7cafd17fbcc87402d90140331b373cf3bf8a
habibor144369 <onilhabib1@gmail.com>
2022-11-29 18:39:36 +0600

    ok

--- function in C/recape_function/program-5.c
+++ function in C/recape_function/program-5.c
@@ -1,12 +1,20 @@
-
+//built in function  in c program..
 
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
 
 int main() {
     printf("%0.2f \n", pow(3, 2));
 
     int a = 3, b = 2;
-    printf("%0.2f", pow(a, b));
+    printf("%0.2f \n", pow(a, b));
+
+    printf("%0.2f \n", sqrt(49));
+
+    printf("%d \n", abs(-12));
+
+    printf("%0.2f \n", sin(12));
+
 return 0;
 }
