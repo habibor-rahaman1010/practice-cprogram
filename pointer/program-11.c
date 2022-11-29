dce36f997ba62fc1f557082264c2f5013cce21cb
habibor144369 <onilhabib1@gmail.com>
2022-11-29 21:59:05 +0600

    ok done

--- pointer/program-11.c
+++ pointer/program-11.c
@@ -3,7 +3,12 @@
 #include <stdio.h>
 
 int main() {
+    int ara[10] = {12, 34, 35, 56, 67, 78, 87, 99, 43, 54};
 
+    int i;
+    for(i = 0; i < 10; i++){
+        printf("%d  %p\n", *(ara + i), (ara + i));
+    }
 
 return 0;
 }
