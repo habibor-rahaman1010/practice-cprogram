ffac6170c76e656ba21fd78c95139d2da8a5bcb2
habibor144369 <onilhabib1@gmail.com>
2022-11-30 19:17:24 +0600

    ok done

--- pointer/problem_solveing/program-11.c
+++ pointer/problem_solveing/program-11.c
@@ -1,8 +1,20 @@
+//special number of c program...
 
 #include <stdio.h>
 
 int main() {
+    int n;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
 
+    int ans = sum_of_digit();
+
+    if(is_prime(ans) == 1){
+        printf("Special Number");
+    }
+    else{
+        printf("Not Special Number");
+    }
 
 return 0;
 }
