87b9045aa3e44ce2685b04c98bf147606d834458
habibor144369 <onilhabib1@gmail.com>
2022-11-30 17:25:01 +0600

    ok programmer

--- pointer/problem_solveing/program-8.c
+++ pointer/problem_solveing/program-8.c
@@ -1,1 +1,21 @@
+// print pattern in c program...
 
+#include <stdio.h>
+
+void print_hash(int n) {
+    for(int i = 1; i <= n; i++){
+        printf("%d ", i);
+    }
+    printf("\n");
+
+return;
+}
+
+int main() {
+
+    for(int i = 1; i <= 5; i++){
+        print_hash(i);
+    }
+
+return 0;
+}
