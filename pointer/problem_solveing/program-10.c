3a6e81b1066a5da391dd97f6e4518e9d41619882
habibor144369 <onilhabib1@gmail.com>
2022-11-30 17:51:06 +0600

    Hello Programmer

--- pointer/problem_solveing/program-10.c
+++ pointer/problem_solveing/program-10.c
@@ -3,9 +3,28 @@
 
 #include <stdio.h>
 
+void find_max_min(int ara[], int len, int *p1, int *p2) {
+    *p1 = ara[0];
+    *p2 = ara[0];
+
+    for(int i = 0; i < len; i++){
+        if(ara[i] > *p1){
+            *p1 = ara[i];
+        }
+        else if(ara[i] < *p2){
+            *p2 = ara[i];
+        }
+    }
+}
 
 int main() {
+    int number[] = {45, 23, 77, 12, 67, 78, 99, 43, 41, 70};
+    int len = sizeof(number) / sizeof(number[0]);   // int lne = sizeof(number) / sizeof(int);
 
+    int maxx, minn;
+    int *p1 = &maxx, *p2 = minn;
+
+    find_max_min(number, len, p1, p2);
 
 
 return 0;
