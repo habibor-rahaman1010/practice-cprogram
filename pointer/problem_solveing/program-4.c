d6fb0d63b5a86292dba469ee5b9c115da00876ce
habibor144369 <onilhabib1@gmail.com>
2022-11-30 14:06:20 +0600

    ok done

--- pointer/problem_solveing/program-4.c
+++ pointer/problem_solveing/program-4.c
@@ -1,1 +1,18 @@
 //You are given an integer n . Now print n to 1 . Implement it using recursion.
+
+#include <stdio.h>
+
+void recursion(int n) {
+    recursion(n - 1);
+    printf("%d ", n);
+}
+
+int main() {
+    int n;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    recursion(n);
+
+return 0;
+}
