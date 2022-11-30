035220c1024a9d7e5bf079faefab44c6f11e5e79
habibor144369 <onilhabib1@gmail.com>
2022-11-30 14:53:16 +0600

    Hello hard core programmer

--- pointer/problem_solveing/program-6.c
+++ pointer/problem_solveing/program-6.c
@@ -1,1 +1,12 @@
+/*
+You are given an integer n . Now print the sum of first n natural numbers.
+For example n=5 , that means sum of first n natural number is 15(1+2+3+4+5) . Implement it using recursion.
+*/
 
+#include <stdio.h>
+
+int main() {
+
+
+return 0;
+}
