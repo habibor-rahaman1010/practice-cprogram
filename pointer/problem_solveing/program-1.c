f66965063e76dd94bde8b232b188bdbfae86f675
habibor144369 <onilhabib1@gmail.com>
2022-11-30 11:40:04 +0600

    ok

--- pointer/problem_solveing/program-1.c
+++ pointer/problem_solveing/program-1.c
@@ -1,1 +1,24 @@
+/*
+You are given a positive integer n. The second line will contain n positive integers. The third line will contain a value k.
+Now print the total count of strictly less and strictly greater value from k. Implement
+*/
 
+#include <stdio.h>
+
+void myFunc(ara[], n) {
+
+
+return 0;
+}
+
+int main() {
+    int n;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    int ara[n];
+    myFunc(ara, n);
+
+
+return 0;
+}
