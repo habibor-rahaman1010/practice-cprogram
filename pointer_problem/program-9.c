19e896b56259442bcc5ddfa93e00effda0212b80
habibor-rahaman1010 <habibor144369@gmmail.com>
2022-12-02 00:24:52 +0600

    ok done

--- pointer_problem/program-9.c
+++ pointer_problem/program-9.c
@@ -1,1 +1,33 @@
+/*
+We give you a string. In string, every character have decimal value. If the decimal value of character is even then
+make the character in uppercase. Implement this by using function.
+*/
 
+#include <stdio.h>
+
+void replace_atring(char str[]) {
+    int i = 0;
+
+    while(str[i] != '\0'){
+        if(str[i] % 2 == 0){
+            printf("%c", str[i] - 32);
+        }
+        else{
+            printf("%c", str[i]);
+        }
+
+        i++;
+    }
+
+return;
+}
+
+int main() {
+    char str[100];
+    printf("Enter your string: ");
+    scanf("%s", str);
+
+    replace_atring(str);
+
+return 0;
+}
