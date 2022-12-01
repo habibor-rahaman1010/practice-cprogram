0a76da40353b8f705a930831b6d15ba02722b663
habibor-rahaman1010 <habibor144369@gmmail.com>
2022-12-01 23:47:32 +0600

    ok done

--- pointer_problem/program-6.c
+++ pointer_problem/program-6.c
@@ -1,1 +1,25 @@
+/*
+You have given positive integer N, the size of an array as input. Take an array of size N as input. Now your task to
+print this array. Implement this by using pointers.
+*/
+#include <stdio.h>
 
+int main() {
+    int n;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    int i;
+    int ara[n];
+    int *p1 = ara;
+
+    for(i = 0; i < n; i++){
+        scanf("%d", (p1 + i));
+    }
+
+    for(i = 0; i < n; i++){
+        printf("%d ", *(p1 + i));
+    }
+
+return 0;
+}
