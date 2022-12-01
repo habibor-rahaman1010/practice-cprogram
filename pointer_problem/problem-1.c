8d703e224f7a495a83dda6224d4c76da47aa8ad0
habibor144369 <onilhabib1@gmail.com>
2022-12-01 21:36:41 +0600

    ok done

--- pointer_problem/problem-1.c
+++ pointer_problem/problem-1.c
@@ -3,7 +3,17 @@
 #include <stdio.h>
 
 int main() {
+    int num1, num2;
+    float avg = 0;
 
+    printf("Enter the value of num1 and num2: ");
+
+    int *p1 = &num1, *p2 = &num2;
+    float *p3 = &avg;
+    scanf("%d %d", p1, p2);
+
+    *p3 = (*p1 + *p2) / 2.0;
+    printf("%0.2f \n", *p3);
 
 return 0;
 }
