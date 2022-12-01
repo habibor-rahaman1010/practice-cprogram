17050234d56880583308f99cf00b4a88209cd7e6
habibor-rahaman1010 <habibor144369@gmmail.com>
2022-12-01 22:21:27 +0600

    ok

---
+++ pointer_problem/program-2.c
@@ -0,0 +1,13 @@
+/*
+In Bubble sort we compares two adjacent elements and swaps them until they are in the intended order.
+You have given positive integer N, the size of an array as input. Take an array of size N as input and sort this array
+by using Bubble sort. You must make a function for swapping numbers by using pass by reference.
+*/
+
+// array sort use bubble sort in c program...
+
+#include <stdio.h>
+int main() {
+    int n;
+    int ara = give_array(n);
+}
