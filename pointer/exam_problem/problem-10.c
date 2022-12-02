494a50001bf7d41e0ad174c1c8c031d8e62a6233
habibor-rahaman1010 <habibor144369@gmmail.com>
2022-12-03 00:04:05 +0600

    ok

--- pointer/exam_problem/problem-10.c
+++ pointer/exam_problem/problem-10.c
@@ -1,1 +1,17 @@
 
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    int ara[n];
+    printf("Enter element of an array: ");
+    for(i = 0; i < n; i++){
+        scanf("%d", &ara[i]);
+    }
+
+
+return 0;
+}
