534c24198644793ff18aa29f96a8fea2eeb197e9
habibor-rahaman1010 <habibor144369@gmmail.com>
2022-12-02 15:23:22 +0600

    ok

---
+++ pointer/exam_problem/problem-5.c
@@ -0,0 +1,33 @@
+
+#include <stdio.h>
+
+int do_sum(int ara[], int n) {
+    int i, sum = 0;
+
+    for(i = 1; i <= n; i++){
+        if(ara[i] % 2 != 0 && i % 2 != 0){
+            sum = ara[i] + i;
+        }
+        else{
+            sum = ara[i] + i;
+        }
+    }
+    printf("%d", sum);
+    //return sum;
+}
+
+int main() {
+    int n, i;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    int ara[n];
+    for(i = 1; i <= n; i++){
+        scanf("%d", &ara[i]);
+    }
+
+    int result = do_sum(ara, n);
+    printf("%d \n", result);
+
+return 0;
+}
