78ffb7786d89fb54f35a67f114a2ce000b8545c9
habibor-rahaman1010 <habibor144369@gmmail.com>
2022-12-02 14:20:19 +0600

    ok

--- pointer/exam_problem/problem-3.c
+++ pointer/exam_problem/problem-3.c
@@ -1,1 +1,18 @@
+// pointer problem in c program...
+
+#include <stdio.h>
+
+int main() {
+    int x, y;
+    printf("Enter the value id (x and y): ");
+    scanf("%d %d", &x, &y);
+
+    int *ptrX = &x, *ptrY = &y;
+    *ptrX = x;
+    *ptrY = y;
+
+    printf("%d %d \n", *ptrX, *ptrY);
+
+return 0;
+}
 
