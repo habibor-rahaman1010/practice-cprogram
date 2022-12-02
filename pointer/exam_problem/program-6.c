11c7380467a01c46710f24f0e3ec3e3c109a6297
habibor-rahaman1010 <habibor144369@gmmail.com>
2022-12-02 16:46:13 +0600

    ok

--- pointer/exam_problem/program-6.c
+++ pointer/exam_problem/program-6.c
@@ -2,12 +2,60 @@
 
 #include <stdio.h>
 
+//divisible by 3
+int divisible3(int ara[], int n) {
+    int i, count3 = 0;
+    for(i = 0; i < n; i++){
+        if(ara[i] % 3 == 0){
+            count3++;
+        }
+    }
+    if(count3 > 0){
+        printf("%d ", count3);
+        return count3;
+    }
+    else{
+        return -1;
+    }
+}
+
+//divisible by 5
+int divisible5(int ara[], int n) {
+    int i, count5 = 0;
+    for(i = 0; i < n; i++){
+        if(ara[i] % 5 == 0){
+            count5++;
+        }
+    }
+    if(count5 > 0){
+        printf("%d\n", count5);
+        return count5;
+    }
+    else{
+        return -1;
+    }
+}
+
 int main() {
     int n;
     printf("Enter the value of n: ");
     scanf("%d", &n);
 
     if(n >= 1 && n <= 100){
+        int ara[n], i;
+        for(i = 0; i < n; i++){
+            scanf("%d", &ara[i]);
+        }
+
+        int result = divisible3(ara, n);
+        int result2 = divisible5(ara, n);
+
+        if(result == -1 && result2 == -1){
+            printf("%d \n", -1);
+        }
+        else{
+            printf("%d \n", result + result2);
+        }
 
     }
     else{
