5010a814f82929d20eaf4289e8b06ddfeafa0252
habibor-rahaman1010 <habibor144369@gmmail.com>
2022-12-02 16:06:00 +0600

    ok

--- pointer/exam_problem/problem-5.c
+++ pointer/exam_problem/problem-5.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 int do_sum(int ara[], int n) {
-    int i, sum = 0, even_sum = 0, odd_sum = 0;
+    int i, sum = 0;
 
     for(i = 1; i <= n; i++){
 
@@ -24,13 +24,18 @@ int main() {
     printf("Enter the value of n: ");
     scanf("%d", &n);
 
-    int ara[n];
-    for(i = 1; i <= n; i++){
-        scanf("%d", &ara[i]);
-    }
+    if(n >= 1 && n <= 100){
+        int ara[n];
+        for(i = 1; i <= n; i++){
+            scanf("%d", &ara[i]);
+        }
 
-    int result = do_sum(ara, n);
-    printf("%d \n", result);
+        int result = do_sum(ara, n);
+        printf("%d \n", result);
+    }
+    else{
+        printf("Constraints (n >= 1 && n <= 100) \n");
+    }
 
 return 0;
 }
