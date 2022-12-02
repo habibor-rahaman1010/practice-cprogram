816d43376023b03baa7251384b54be81541026b4
habibor-rahaman1010 <habibor144369@gmmail.com>
2022-12-02 16:10:26 +0600

    ok

--- pointer/exam_problem/program-6.c
+++ pointer/exam_problem/program-6.c
@@ -1,1 +1,18 @@
+//Divisible 3 or Divisible by 5 and total divisor cont in c program...
 
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    if(n >= 1 && n <= 100){
+
+    }
+    else{
+        printf("Constraints is: (n >= 1 && n <= 100) \n");
+    }
+
+return 0;
+}
