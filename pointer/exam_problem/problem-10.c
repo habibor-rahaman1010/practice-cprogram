700367a98943d47cf2bbc213e3f37664d1548bca
habibor-rahaman1010 <habibor144369@gmmail.com>
2022-12-03 11:40:21 +0600

    ok

--- pointer/exam_problem/problem-10.c
+++ pointer/exam_problem/problem-10.c
@@ -2,10 +2,11 @@
 #include <stdio.h>
 
 int is_beautiful(int *ara, int *n) {
-    int i, j, counter = 0;
+    int i = 0, j, counter = 0;
+    int *a = &ara[0], half = *n / 2;
 
     for(i = 0; i < *n; i++){
-        for(j = 0; j < 2; j++){
+        for(j = 0; j < *(a + i); j++){
              if(*(ara + i) % 10 == 7){
                 counter++;
                 *(ara + j) /= 10;
@@ -13,8 +14,38 @@ int is_beautiful(int *ara, int *n) {
         }
     }
 
-    printf("%d ", counter);
+    if(counter >= half){
+        return 1;
+    }
+    else{
+        return 0;
+    }
 }
+
+
+int is_beatiful(int a[], int n)
+{
+   int count=0, i=0, half=n/2;
+   int *ar = &a[0];
+   for(i=0; i<n; i++)
+   {
+      int num = *(ar+i);
+      while(num > 0)
+      {
+         if(num%10==7)
+            count++;
+
+         num/=10;
+      }
+   }
+   if(count >= half)
+      return 1;
+   else
+      return 0;
+}
+
+
+
 
 int main() {
     int n, i;
