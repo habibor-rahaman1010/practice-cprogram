7ce092f82388dec790ce4c52c53e7c0cfe01e283
habibor-rahaman1010 <habibor144369@gmmail.com>
2022-12-04 13:11:53 +0600

    ok

--- more_pointer/program-4.c
+++ more_pointer/program-4.c
@@ -13,4 +13,18 @@ int main() {
 
     b = (double)25 / 2.0; //type casting...
     printf("%lf \n", b);
+
+
+    int x = 45;
+    double y = 67.8;
+
+    int *ptr1;
+    double *ptr2;
+
+    ptr1 = &x;
+    ptr2 = (double*)ptr1;
+
+    printf("%d %lf \n", *ptr1, *ptr2);
+    printf("%p %p \n", ptr1, ptr2);
+
 }
