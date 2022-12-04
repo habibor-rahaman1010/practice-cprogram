19ec2e9acfbe42e5073246c83cc0164467c9a9d9
habibor-rahaman1010 <habibor144369@gmmail.com>
2022-12-04 14:13:58 +0600

    ok

--- more_pointer/program-7.c
+++ more_pointer/program-7.c
@@ -1,10 +1,26 @@
-
+// dynamic memory allocation in c program....
 
 #include <stdio.h>
-
+#include <stdlib.h>
 
 int main() {
+    int n;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
 
+    int *ptr;
+    ptr = (int *) malloc(n * sizeof(int));
+
+    if(ptr == NULL){
+        printf("Memory allocation is failed for some reason!");
+    }
+    else{
+        int i;
+        printf("Enter element of array: ");
+        for(i = 0; i < n; i++){
+            scanf("%d", &ptr[i]);
+        }
+    }
 
 return 0;
 }
