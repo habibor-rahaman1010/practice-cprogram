b9ee8fb7151af6b2e6027bd9a198a2462f1929fa
habibor-rahaman1010 <habibor144369@gmmail.com>
2022-12-05 11:44:12 +0600

    ok done programmer

--- structure/program-1.c
+++ structure/program-1.c
@@ -2,8 +2,20 @@
 
 #include <stdio.h>
 
-int main() {
+struct Student {
+    int id;
+    int age;
+    float weight;
+};
 
+int main() {
+    struct Student s = {
+        144369,
+        22,
+        55.0
+    };
+
+    printf("%d", s.age);
 
 return 0;
 }
