4069a1d2d8f58bef685acaf934ca54b3773a93ee
habibor-rahaman1010 <habibor144369@gmmail.com>
2022-12-05 13:39:14 +0600

    ok done programmer

--- structure/program-9.c
+++ structure/program-9.c
@@ -1,1 +1,24 @@
+//Structure in c program...
+
+#include <stdio.h>
+
+struct Student {
+    int id;
+    char *name;
+    int age;
+    char *gender;
+    float weight;
+};
+
+int main() {
+    struct Student s = {.name = "Habibor Rahaman", .age = 22, .gender = "Meal", .weight = 54.5 .id = 144369};
+
+    printf("%d\n", s.id);
+    printf("%s\n", s.name);
+    printf("%d\n", s.age);
+    printf("%s\n", s.gender);
+    printf("%0.2f\n", s.weight);
+
+return 0;
+}
 
